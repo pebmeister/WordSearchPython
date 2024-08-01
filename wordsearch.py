3d47ec985798d26200f9185d3f2c770f49f3c830
Paul Baxter <jareth_2005@yahoo.com>
2024-08-02 00:35:06 +1000

    general code clean up. use try except for argument parser

--- wordsearch.py
+++ wordsearch.py
@@ -4,13 +4,16 @@ Created on Mon Jul 22 13:20:11 2024
 
 @author: Paul Baxter
 """
-import random, argparse
+import random
+import argparse
 import numpy as np
 
-class wordsearch:
+
+class WordSearch:
+    failed_word: str
 
     # initialize
-    def __init__(self, rows, cols, words):
+    def __init__(self, rows: int, cols: int, words: list):
         self.rows = rows
         self.cols = cols
         self.words = np.array(words)
@@ -24,13 +27,13 @@ class wordsearch:
         # row col offsets for directions
         self.row_col_offsets = [
             [-1, 0],
-            [ 1, 0],
-            [ 0,-1],
-            [ 0, 1],
-            [-1,-1],
+            [1, 0],
+            [0, -1],
+            [0, 1],
+            [-1, -1],
             [-1, 1],
-            [ 1,-1],
-            [ 1, 1]]
+            [1, -1],
+            [1, 1]]
 
         self.clear_board()
         self.add_words(self.words)
@@ -38,6 +41,7 @@ class wordsearch:
     # -------------------------------------------------------------
 
     def clear_board(self):
+        # * represents a blank spot in the board
         for row in range(self.rows):
             for col in range(self.cols):
                 self.board[row][col] = '*'
@@ -55,44 +59,50 @@ class wordsearch:
     # -------------------------------------------------------------
 
     # check if a word fits in the given position and direction
-    def word_fits(self, word, row, col, direction):
+    def word_fits(self, word: str, row: int, col: int, direction: int) -> bool:
+        # assign current row and column
         rr = row
         cc = col
 
-        rinc = self.row_col_offsets[direction][0]
-        cinc = self.row_col_offsets[direction][1]
+        # get the row and column increment
+        row_inc = self.row_col_offsets[direction][0]
+        col_inc = self.row_col_offsets[direction][1]
 
+        # loop through each letter
         for ch in word:
+            # make sure we are still on the board
             if rr < 0 or rr >= self.rows:
                 return False
             if cc < 0 or cc >= self.cols:
                 return False
+
+            # check if letter is the same or blank
             c = self.board[rr][cc]
             if ch != c and c != '*':
                 return False
 
-            rr += rinc
-            cc += cinc
+            # increment row and column
+            rr += row_inc
+            cc += col_inc
 
         return True
 
     # -------------------------------------------------------------
 
     # place a word in puzzle
-    # must first call wordfits
+    # must first call word_fits
     def place_word(self, word, row, col, direction):
-        rinc = self.row_col_offsets[direction][0]
-        cinc = self.row_col_offsets[direction][1]
+        row_inc = self.row_col_offsets[direction][0]
+        col_inc = self.row_col_offsets[direction][1]
         for ch in word:
             self.board[row][col] = ch
-            row += rinc
-            col += cinc
+            row += row_inc
+            col += col_inc
 
     # -------------------------------------------------------------
 
     # add one word to the board in a random location and direction
-    def add_word(self, word):
-
+    def add_word(self, word: str) -> bool:
         random.shuffle(self.row_array)
         random.shuffle(self.col_array)
         random.shuffle(self.dir_array)
@@ -100,7 +110,7 @@ class wordsearch:
         for row in self.row_array:
             for col in self.col_array:
                 for d in self.dir_array:
-                    if self.word_fits(word, row, col, d) == True:
+                    if self.word_fits(word, row, col, d):
                         self.place_word(word, row, col, d)
                         return True
 
@@ -115,12 +125,11 @@ class wordsearch:
     def add_words(self, words):
 
         # words tend to fit easier if long words are added first
-        srt_words = np.array(sorted(words, key=len, reverse=True))
+        word: str
+        for word in np.array(sorted(words, key=len, reverse=True)):
+            word_added: bool = self.add_word(word)
 
-        for word in srt_words:
-            word_added = self.add_word(word)
-
-            if word_added == False:
+            if not word_added:
                 return False
         return True
 
@@ -132,50 +141,64 @@ class wordsearch:
             for i in range(margin):
                 print('', end=' ')
             for col in range(self.cols):
-                print(self.board[row][col], end= ' ')
+                print(self.board[row][col], end=' ')
             print()
 
     # -------------------------------------------------------------
 
     def print_words(self, word_width, num_columns):
-        # get the length of the longest word to make sure all words fit
-        srt_words = np.array(sorted(self.words, key=len, reverse=True))
-        sz = len(srt_words[0])
 
         col = 0
         for word in self.words:
-            l = word_width - len(word)
+            l: int = word_width - len(word)
             print(word, end='')
             for i in range(l):
                 print(' ', end='')
-            if col == num_columns -1:
+            if col == num_columns - 1:
                 print()
                 col = 0
                 continue
             col += 1
 
     # -------------------------------------------------------------
+
+
 def main():
     # create parser
-    descStr = "This program creates a wordsearch."
-    parser = argparse.ArgumentParser(prog='wordsearch', description=descStr)
+    desc_str = "This program creates a wordsearch."
+    parser = argparse.ArgumentParser(prog='wordsearch', description=desc_str)
     # add expected arguments
     parser.add_argument('-cols', dest='cols', type=int, required=True, help='number of columns')
     parser.add_argument('-rows', dest='rows', type=int, required=True, help='number of rows')
-    parser.add_argument('-tries', dest='tries', type=int, default=1, required=False, help='max number of attempts to make puzzle')
+    parser.add_argument('-tries', dest='tries', type=int, default=1, required=False,
+                        help='max number of attempts to make puzzle')
     parser.add_argument('-margin', dest='margin', type=int, default=12, required=False, help='left margin for puzzle')
-    parser.add_argument('-ww', dest='word_width', type=int, default=20, required=False, help='width for column of word list')
-    parser.add_argument('-wc', dest='word_cols', type=int, default=4, required=False, help='number of columns for word list')
+    parser.add_argument('-ww', dest='word_width', type=int, default=20, required=False,
+                        help='width for column of word list')
+    parser.add_argument('-wc', dest='word_cols', type=int, default=4, required=False,
+                        help='number of columns for word list')
     parser.add_argument(dest="words", nargs='+', type=str, help='words to add')
 
     # parse args
-    args = parser.parse_args()
+    try:
+        args = parser.parse_args()
 
-    # try up to 20 times to make the word search
+    except argparse.ArgumentError:
+        print('Argument error. Unable to parse arguments.')
+        parser.print_help()
+        return
+
+    except argparse.ArgumentTypeError:
+        print('Argument type error. Unable to parse arguments.')
+        parser.print_help()
+        return
+
+    word = ''
+    # try to make the word search
     for tries in range(args.tries):
 
         # create the wordsearch
-        ws = wordsearch(args.rows, args.cols, args.words)
+        ws = WordSearch(args.rows, args.cols, args.words)
 
         # check for an error
         if not ws.error:
@@ -195,6 +218,7 @@ def main():
 
     # -------------------------------------------------------------
 
+
 # call main
 if __name__ == '__main__':
-  main()
+    main()
